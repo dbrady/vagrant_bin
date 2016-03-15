8dd5b4c97fa8f72e19874c24834ec49ebe81db16
David Brady <dbrady@shinybit.com>
2016-03-15 15:11:54 -0600

    Add TODOs to setup.sh
    Poor man's README-driven development FTW

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# setup.sh is intended to be a sort of puppet-like setter-upper; when (sadly,
+# not if) vagrant barfs and I have to set up a new box, run this and it will add
+# everything back in that I currently have missing.
+
+# TODO: Set up my dotfiles (.bashrc, .aliases, .bash_completions, .ackrc, etc)
+
+# Install nix package manager
 # TODO: wrap this in "if nix-env not installed, then..."
 # curl https://nixos.org/nix/install | sh
 
